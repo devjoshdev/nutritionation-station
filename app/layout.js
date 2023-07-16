593f1164a0c35b934ec5b9f063ea0cf986bc4203
devjoshdev <jsinanan1@gmail.com>
2023-07-16 19:12:52 -0400

    Change background color globally

--- app/layout.js
+++ app/layout.js
@@ -7,10 +7,14 @@ export const metadata = {
   description: 'The Nutritionation Lookup Station is a simple food look-up service using the FoodData Central food database',
 }
 
+const globalStyle = {
+  backgroundColor: "#ECE9D6",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} style={globalStyle}>{children}</body>
     </html>
   );
 }
