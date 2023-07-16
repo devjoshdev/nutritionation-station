f48660721461742ccbf1509268602c7ce833c2c7
devjoshdev <jsinanan1@gmail.com>
2023-07-15 23:51:32 -0400

    Start on search

--- app/page.js
+++ app/page.js
@@ -1,8 +1,19 @@
+"use client";
+import Food from "@/components/Food";
+import { useState } from "react";
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleInputChange = (e) => setSearchTerm(e.target.value);
+  console.log("input is", searchTerm);
   return (
     <div>
-      <h1>Hello, World</h1>
+      <h1>The Nutritionation Lookup Station</h1>
+      <p>Search for foods and get results</p>
+      <input placeholder="Start your search here" value={searchTerm} onChange={handleInputChange}/>
+      <div id="results">
+        <Food/>
+      </div>
     </div>
   );
 }
