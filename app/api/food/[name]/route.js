1e2d0539373b18215755079c6a4454d1012cbb7c
devjoshdev <jsinanan1@gmail.com>
2023-07-17 00:11:06 -0400

    Add in query to grab relevant data and return it

--- app/api/food/[name]/route.js
+++ app/api/food/[name]/route.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
     keepAliveInitialDelay: 0
 }).promise();
 export async function GET(req, {params}) {
-    const [results] = await pool.query("SELECT * FROM `food` WHERE `name` LIKE ?", ["%" + params.name + "%"]);
+    const [results] = await pool.query("SELECT * FROM `serving` s INNER JOIN `food` f ON s.food_id = f.id INNER JOIN `calorie` c ON c.food_id = f.id", ["%" + params.name + "%"]);
     const numResults = results.length;
     console.log(numResults);
     return NextResponse.json(results);
