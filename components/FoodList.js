9d00601f3074774f7a476d6f966e3ff02efaf6dd
devjoshdev <jsinanan1@gmail.com>
2023-07-16 22:27:24 -0400

    Change servings to be an object

--- components/FoodList.js
+++ components/FoodList.js
@@ -1,7 +1,7 @@
 const FoodList = ({foods}) => {
     return (
         <div>
-            {foods.map(food => <Food key={food.id} name={food.name} desc={food.desc} servings={food.servings} calsPer={food.cals}/>)}
+            {foods.map(food => <Food key={food.id} name={food.name} servings={food.servings} calsPer={food.cals}/>)}
         </div>
     );
 };
