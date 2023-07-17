2645c7c6b465b0b2a62381868e95d9739b4e2efe
devjoshdev <jsinanan1@gmail.com>
2023-07-16 23:00:50 -0400

    Query DB for GET param

--- app/api/food/[name]/route.js
+++ app/api/food/[name]/route.js
@@ -1,5 +1,19 @@
 import { NextResponse } from "next/server";
-
+import mysql from "mysql2";
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'rootroot',
+    port: 3306,
+    database: 'test',
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
+}).promise();
 export async function GET(req, {params}) {
-    return NextResponse.json(params.name);
+    const [results] = await pool.query("SELECT * FROM `food` WHERE `name` LIKE ?", ["%" + params.name + "%"]);
+    const numResults = results.length;
+    console.log(numResults);
+    return NextResponse.json(results);
 };
