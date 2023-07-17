f28cf90bdc35e8b92a6ac0718ed87592309fdbef
devjoshdev <jsinanan1@gmail.com>
2023-07-17 00:11:46 -0400

    Connect to frontend to backend but still in use as a testing page for now

--- app/page.js
+++ app/page.js
@@ -1,18 +1,28 @@
 "use client";
-import Food from "@/components/Food";
-import { useState } from "react";
-
+import Food from "@/components/Food.js";
+import { useEffect, useState } from "react";
+const servingsData = [{id: 1, desc: "1 cube", cals: 28}, {id: 2, desc: "1 block", cals: 558}];
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [foodData, setFoodData] = useState([]);
   const handleInputChange = (e) => setSearchTerm(e.target.value);
-  console.log("input is", searchTerm);
+  useEffect(() => {
+    if (searchTerm === "" || searchTerm.length < 2) return;
+    async function grabFoods() {
+      const results = await fetch(`api/food/${searchTerm}`, {cache: 'no-store'});
+      const data = await results.json();
+      setFoodData(data);
+    }
+    grabFoods();
+  }, [searchTerm]);
+  console.log(foodData);
   return (
     <div>
       <h1>The Nutritionation Lookup Station</h1>
       <p>Search for foods and get results</p>
       <input placeholder="Start your search here" value={searchTerm} onChange={handleInputChange}/>
       <div id="results">
-        <Food/>
+        <Food name="Cheese" servings={servingsData} calsPer={100}/>
       </div>
     </div>
   );
