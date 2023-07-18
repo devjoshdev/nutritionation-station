d937793f21fb5801b9e4f594912fb1deb8ec8b97
devjoshdev <jsinanan1@gmail.com>
2023-07-18 00:09:28 -0400

    Modify db result shape to fit what is expected on the client

--- app/api/food/[name]/route.js
+++ app/api/food/[name]/route.js
@@ -12,6 +12,16 @@ const pool = mysql.createPool({
     keepAliveInitialDelay: 0
 }).promise();
 export async function GET(req, {params}) {
+    let foodMap = new Map();
     const [results] = await pool.query("SELECT f.id, f.name, s.modifier, s.gram_weight, c.amount from food f INNER JOIN serving s ON s.food_id = f.id INNER JOIN calorie c ON c.food_id = f.id WHERE f.name LIKE ?", ["%" + params.name + "%"]);
-    return NextResponse.json(results);
+    results.forEach(result => {
+        if (foodMap.has(result.id)) {
+            const currentVal = foodMap.get(result.id);
+            currentVal.servings.push({modifier: result.modifier, gram_weight: result.gram_weight,});
+        }
+        else {
+            foodMap.set(result.id, {id: result.id, name: result.name, amount: result.amount, servings: [{modifier: result.modifier, gram_weight: result.gram_weight,}],});
+        }
+    });
+    return NextResponse.json(Array.from(foodMap.values()));
 };
