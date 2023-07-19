08846aefeaa6fccfdadc7eee6bfe009490009ad4
devjoshdev <jsinanan1@gmail.com>
2023-07-19 01:54:24 -0400

    Initiate search from button click or pressing enter while input in focus

--- app/page.js
+++ app/page.js
@@ -6,21 +6,22 @@ export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
   const [foodData, setFoodData] = useState([]);
   const handleInputChange = (e) => setSearchTerm(e.target.value);
-  useEffect(() => {
-    if (searchTerm === "" || searchTerm.length < 2) return;
-    async function grabFoods() {
-      const results = await fetch(`api/food/${searchTerm}`, {cache: 'no-store'});
-      const data = await results.json();
-      setFoodData(data);
-    }
-    grabFoods();
-  }, [searchTerm]);
+  const handleKeyPressForSearch = (e) => {if (e.key === "Enter") performSearch();}
+  const performSearch = async () => {
+    console.log("search term is", searchTerm, `api/food/${searchTerm}`);
+    if (searchTerm.length < 3) return;
+    const results = await fetch(`api/food/${searchTerm}`, {cache: 'no-store'});
+    const data = await results.json();
+    setFoodData(data);
+  };
+
   console.log(foodData);
   return (
     <div>
       <h1>The Nutritionation Lookup Station</h1>
       <p>Search for foods and get results</p>
-      <input placeholder="Start your search here" value={searchTerm} onChange={handleInputChange}/>
+      <input placeholder="Start your search here" value={searchTerm} onChange={handleInputChange} onKeyDown={handleKeyPressForSearch}/>
+      <button onClick={performSearch}>Search</button>
       <div id="results">
         <Food name="Cheese" servings={servingsData} calsPer={100}/>
       </div>
