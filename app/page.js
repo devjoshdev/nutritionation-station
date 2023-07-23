be539c7a0be7784b6ed10c25a9c6cb7e3b1fc6fc
devjoshdev <jsinanan1@gmail.com>
2023-07-23 16:25:52 -0400

    Error check fetch call and set error message appropriately

--- app/page.js
+++ app/page.js
@@ -15,19 +15,37 @@ const elemStyle = {
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
   const [foodData, setFoodData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
   const handleInputChange = (e) => setSearchTerm(e.target.value);
   const handleKeyPressForSearch = (e) => {if (e.key === "Enter") performSearch();}
   const performSearch = async () => {
     console.log("search term is", searchTerm, `api/food/${searchTerm}`);
     if (searchTerm.length < 3) return;
-    const results = await fetch(`api/food/${searchTerm}`, {cache: 'no-store'});
-    const data = await results.json();
-    setFoodData(data);
+    try {
+      const results = await fetch(`api/food/${searchTerm}`, {cache: 'no-store',});
+      if (!results.ok) {
+        setErrorMessage("Something went wrong! Try again when this message disappears");
+        setTimeout(() => {
+          setErrorMessage("");
+        }, 5000);
+      }
+      const data = await results.json();
+      setFoodData(data);
+    }
+    catch (err) {
+      setFoodData([]);
+      setErrorMessage("There was a network error");
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 5000);
+    }
+
   };
 
   console.log(foodData);
   return (
     <div style={elemStyle}>
+      {errorMessage !== "" ? <h1 style={{color: "red",}}>{errorMessage}</h1> : ""}
       <h1 style={textStyle}>The Nutritionation Lookup Station</h1>
       <p style={textStyle}>Search for foods and get results</p>
       <input placeholder="Start your search here" value={searchTerm} onChange={handleInputChange} onKeyDown={handleKeyPressForSearch}/>
